f09e1b9a28d4c88459c039d7392466f556516199
kevinbs26 <kevinbachtiar26@users.github.com>
2020-12-17 00:33:54 +0700

    benerin seeder & documentation

--- sokopedia/database/seeds/ProductSeeder.php
+++ sokopedia/database/seeds/ProductSeeder.php
@@ -12,41 +12,41 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-       
+
         Product::create([
             'name' => 'Xiaomi TV',
             'description' => 'Xiaomi Tv is coolest tv in the world',
             'price' => 1200000,
             'image' => 'xiaomi_TV.jpg',
-            'category_id' => rand(1, 3)
+            'category_id' => 2
         ]);
         Product::create([
-            'name' => 'Xiaomi TV',
-            'description' => 'Xiaomi Tv is coolest tv in the world',
-            'price' => 1200000,
+            'name' => 'Samsung TV',
+            'description' => 'Samsung Tv has best curve display in the world',
+            'price' => 3000000,
             'image' => 'xiaomiTV.jpg',
-            'category_id' => rand(1, 3)
+            'category_id' => 2
         ]);
         Product::create([
-            'name' => 'Xiaomi TV',
-            'description' => 'Xiaomi Tv is coolest tv in the world',
-            'price' => 1200000,
+            'name' => 'Iphone 12',
+            'description' => 'Its a leap year',
+            'price' => 15000000,
             'image' => 'xiaomiTV.jpg',
-            'category_id' => rand(1, 3)
+            'category_id' => 1
         ]);
         Product::create([
-            'name' => 'Xiaomi TV',
-            'description' => 'Xiaomi Tv is coolest tv in the world',
-            'price' => 1200000,
+            'name' => 'Xiaomi 10T',
+            'description' => 'Xiaomi flagship with best performance',
+            'price' => 9000000,
             'image' => 'xiaomiTV.jpg',
-            'category_id' => rand(1, 3)
+            'category_id' => 1
         ]);
         Product::create([
-            'name' => 'Xiaomi TV',
-            'description' => 'Xiaomi Tv is coolest tv in the world',
-            'price' => 1200000,
+            'name' => 'Macbook 2021',
+            'description' => 'New chip m1 much better than the blue past',
+            'price' => 20000000,
             'image' => 'xiaomiTV.jpg',
-            'category_id' => rand(1, 3)
+            'category_id' => 3
         ]);
     }
 }
